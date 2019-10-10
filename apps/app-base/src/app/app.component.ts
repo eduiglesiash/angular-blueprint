5b18d29fe424eb97dbf73b684bb5e5fa560e148f
Alberto Basalo <albertobasalo@hotmail.com>
2019-10-10 13:33:11 +0200

    feat(2-pwa): basic pwa capabilities

--- apps/app-base/src/app/app.component.ts
+++ apps/app-base/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SwUpdate, UpdateAvailableEvent } from '@angular/service-worker';
 
 @Component({
   selector: 'abs-root',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styles: []
 })
 export class AppComponent {
-  title = 'Angular-Blueprint';
+  public title = 'Angular-Blueprint';
+
+  constructor(private swUpdate: SwUpdate) {
+    this.checkVersionUpdates();
+  }
+
+  private checkVersionUpdates() {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.checkForUpdate();
+      this.swUpdate.available.subscribe((event: UpdateAvailableEvent) => {
+        if (event.current.appData) {
+          this.askToUpdateApp(event);
+        }
+      });
+    }
+  }
+
+  private askToUpdateApp(event: UpdateAvailableEvent) {
+    const appData: any = event.current.appData;
+    let msg = `New version ${appData.version} available.`;
+    msg += `${appData.changelog}.`;
+    msg += 'Reaload now?';
+    if (confirm(msg)) {
+      window.location.reload();
+    }
+  }
 }
