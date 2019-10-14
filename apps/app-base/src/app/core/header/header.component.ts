9e7adc049a3951e7b6631276eecb9aafb4e05878
Alberto Basalo <albertobasalo@hotmail.com>
2019-10-14 17:11:56 +0200

    style

--- apps/app-base/src/app/core/header/header.component.ts
+++ apps/app-base/src/app/core/header/header.component.ts
@@ -8,15 +8,7 @@ import {
 @Component({
   selector: 'abs-header',
   templateUrl: './header.component.html',
-  styles: [
-    `
-      section {
-        display: flex;
-        left: 0;
-        justify-content: space-between;
-      }
-    `
-  ],
+  styleUrls: ['./header.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HeaderComponent implements OnInit {
